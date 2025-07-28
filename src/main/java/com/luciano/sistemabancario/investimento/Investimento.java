42ac8e8c41911e04794dc16e89b908bc4aed3ac3
Luciano Fernandes <127912301+LucianoRSFernandes@users.noreply.github.com>
2025-07-27 22:02:32 -0300

    Update Investimento.java

--- src/main/java/com/luciano/sistemabancario/investimento/Investimento.java
+++ src/main/java/com/luciano/sistemabancario/investimento/Investimento.java
@@ -1,36 +1,24 @@
-// src/main/java/com/luciano/sistemabancario/investimento/Investimento.java
 package com.luciano.sistemabancario.investimento;
 
 import com.luciano.sistemabancario.util.MoedaUtil;
-import lombok.Getter; // Gera getters para os campos
-import lombok.NoArgsConstructor; // Necessário para @AllArgsConstructor se for usado
-import lombok.AllArgsConstructor; // Gera construtor com todos os campos
-import lombok.ToString; // Gera toString()
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
-@Getter // Gera getters para todos os campos
-@NoArgsConstructor // Construtor sem argumentos (útil para frameworks)
-@AllArgsConstructor // Construtor com todos os argumentos
-@ToString // Gera o método toString()
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public abstract class Investimento {
     protected double valorAplicado;
     protected LocalDate dataAplicacao;
-    protected double taxaRendimentoDiaria; // Taxa diária
+    protected double taxaRendimentoDiaria;
 
-    // O construtor manual foi removido, pois @AllArgsConstructor o gera
-
-    /**
-     * Calcula o rendimento do investimento.
-     * Este método deve ser implementado pelas subclasses.
-     * @param dias O número de dias para calcular o rendimento.
-     * @return O valor do rendimento.
-     */
     public abstract double calcularRendimento(long dias);
 
-    /**
-     * Imprime os detalhes básicos do investimento.
-     */
     public void imprimirDetalhes() {
         System.out.println("Tipo de Investimento: " + getTipo());
         System.out.println("  Valor Aplicado: " + MoedaUtil.formatar(valorAplicado));
@@ -38,10 +26,5 @@ public abstract class Investimento {
         System.out.println("  Taxa de Rendimento Diária: " + (taxaRendimentoDiaria * 100) + "%");
     }
 
-    /**
-     * Retorna o tipo de investimento (ex: "CDB", "LCI").
-     * Deve ser implementado pelas subclasses.
-     * @return Uma string com o tipo de investimento.
-     */
     public abstract String getTipo();
 }
